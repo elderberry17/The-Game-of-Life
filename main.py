d2c4276bd65e36059614f0b6f600144b4d97d998
Alexey <bezgin14a@mail.ru>
2022-01-10 10:38:36 +0300

    fixed some bugs

--- main.py
+++ main.py
@@ -20,21 +20,22 @@ def play():
             elif set_cells < max_set_cells:
                 if check_click(event):
                     set_cells += 1
-            elif has_changes:
-                if not simulation:
-                    simulation = True
-                pops.append(main_simulator.grid.alive)
-                has_changes = main_simulator.check_n_and_refresh()
-                generation += 1
-                gens.append(generation)
-                sleep(time_to_sleep)
 
-            if has_changes:
-                logging(generation, main_simulator.grid.alive, simulation, max_set_cells - set_cells)
-                draw()
+        if has_changes and set_cells == max_set_cells:
+            if not simulation:
+                simulation = True
+            pops.append(main_simulator.grid.alive)
+            has_changes = main_simulator.check_n_and_refresh()
+            generation += 1
+            gens.append(generation)
+            sleep(time_to_sleep)
 
-            else:
-                show_end(gens, pops)
+        if has_changes:
+            logging(generation, main_simulator.grid.alive, simulation, max_set_cells - set_cells)
+            draw()
+
+        else:
+            show_end(gens, pops)
 
 
 if __name__ == "__main__":
