a4c98251c0ac21f6c9b4a83e7fcc9738e2ad4bfa
Alexey <bezgin14a@mail.ru>
2022-01-10 12:18:53 +0300

    added position args

--- const.py
+++ const.py
@@ -1,7 +1,9 @@
-width, height = 1024, 512
+import sys
+
+width, height = int(sys.argv[1]), int(sys.argv[2])
+max_set_cells = int(sys.argv[3])
 extra_height = 64
 colors_dict = {"back": (245, 240, 225), "lines": (30, 61, 89), "life": (80, 200, 120), "text": (80, 200, 120)}
 cell_size = 32
 FPS = 60
-max_set_cells = 10
 time_to_sleep = 0.35
